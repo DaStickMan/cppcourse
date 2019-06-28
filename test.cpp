3cfeeb8252289ff6f2daaf66dee05c271b967f15
FILIPE OTERO PRADO <filipeop@acs.com.br>
2019-06-28 15:18:10 -0300

    VsCode configs

--- test.cpp
+++ test.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-int main()
+class Estudante
 {
-  cout << "Hello world!" << endl;
+public:
+  char *nome;
+
+  Estudante(const char *nome)
+  {
+    cout << "Nova instância de: " << nome << endl;
+    int tam = strlen(nome) + 1;
+    this->nome = new char[tam];
+    strcpy(this->nome, nome);
+  }
+
+  Estudante(const Estudante &e)
+  {
+    cout << "COPIANDO: " << endl;
+    const char *nmCpy = "Copia de ";
+    int tam = strlen(e.nome) + strlen(nmCpy) + 1;
+    this->nome = new char[tam];
+    strcpy(this->nome, nmCpy);
+    strcat(this->nome, e.nome);
+  }
+
+  const char *getNome()
+  {
+    return nome;
+  }
+};
+
+main()
+{
+  Estudante e("TESTE");
+  Estudante f(e);
+  cout << "COPIA: " << f.getNome() << endl;
 }
